fcfde7fb100c1472366b33e17f73258708b0d49c
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-06-12 04:16:54 -0400

    displaying list of games in search view

---
+++ src/components/Busquedas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(item, index) in traerProductos" :key="index">{{item.codigo}} | {{item.nombre}} | {{item.stock}} | {{item.precio}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+    name: 'Busquedas',
+
+    computed: {
+        // Con la propiedad computada se obtiene la variable actualizada en tiempo real
+        traerProductos(){
+        return this.$store.getters.stockProductos;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
-        <h2>HOLA MUNDO</h2>
+        <h1>{{titulo1}}</h1>
+        <h2>{{subtitulo1}}</h2>
     </div>
 </template>
 <script>
 import {mapState} from 'vuex';
 export default {
+    name: 'Inicio',
+
+    computed: {
+        // mostrarTitulo() {
+        //     return this.$store.state.titulo1;
+        // },
+        // mostrarSubtitulo() {
+        //     return this.$store.state.subtitulo1;
+        // }
+
+        // LLamando a mapState dentro de propiedad computada
+        ...mapState(['titulo1', 'subtitulo1'])
+
+    }
 }
 </script>
